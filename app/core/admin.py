6c3f6487a5d19a7c58729de3b71f0c61ee072434
thenils <nileshnandaniya135@gmail.com>
2021-06-28 22:34:54 +0530

    added test for user admin and updated admin.py file with custom user

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from .models import User
+
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
 # Register your models here.
-admin.site.register(User)
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+admin.site.register(models.User, UserAdmin)
